eaf010cff31fb3e91b666f49effde84cfd6a87ae
Dmitr Tkachenko <mitja-tkachenk@rambler.ru>
2022-02-01 20:38:47 +0300

    Initial commit

---
+++ src/main/java/ru/itsjava/configuration/Config.java
@@ -0,0 +1,23 @@
+package ru.itsjava.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import ru.itsjava.Animal;
+import ru.itsjava.Lamb;
+import ru.itsjava.Piglet;
+
+@Import(OtherConfig.class)
+@Configuration
+public class Config {
+
+    @Bean("myBestPiglet")
+    public Animal piglet(){
+        return new Piglet();
+    }
+
+    @Bean
+    public Animal lamb(){
+        return new Lamb();
+    }
+}
